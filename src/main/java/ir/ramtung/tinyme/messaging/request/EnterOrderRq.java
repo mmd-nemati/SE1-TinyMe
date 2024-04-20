9ffbc101c76b8d10409b3e54d9d2a74e92422106
Aryan Bastani <101284002+AryanBastani@users.noreply.github.com>
2024-04-20 23:48:07 +0330

    Add stopPrice field to EnterOrderRq

--- src/main/java/ir/ramtung/tinyme/messaging/request/EnterOrderRq.java
+++ src/main/java/ir/ramtung/tinyme/messaging/request/EnterOrderRq.java
@@ -27,6 +27,7 @@ public class EnterOrderRq {
     private long shareholderId;
     private int peakSize;
     private int minimumExecutionQuantity = 0;
+    private int stopPrice = 0;
     private EnterOrderRq(OrderEntryType orderEntryType, long requestId, String securityIsin, long orderId, LocalDateTime entryTime, Side side, int quantity, int price, long brokerId, long shareholderId, int peakSize) {
         this.requestType = orderEntryType;
         this.requestId = requestId;
@@ -55,6 +56,25 @@ public class EnterOrderRq {
         this.peakSize = peakSize;
         this.minimumExecutionQuantity = minimumExecutionQuantity;
     }
+
+    private EnterOrderRq(OrderEntryType orderEntryType, long requestId, String securityIsin, long orderId,
+                         LocalDateTime entryTime, Side side, int quantity, int price, long brokerId,
+                         long shareholderId, int peakSize, int minimumExecutionQuantity, int stopPrice) {
+        this.requestType = orderEntryType;
+        this.requestId = requestId;
+        this.securityIsin = securityIsin;
+        this.orderId = orderId;
+        this.entryTime = entryTime;
+        this.side = side;
+        this.quantity = quantity;
+        this.price = price;
+        this.brokerId = brokerId;
+        this.shareholderId = shareholderId;
+        this.peakSize = peakSize;
+        this.minimumExecutionQuantity = minimumExecutionQuantity;
+        this.stopPrice = stopPrice;
+    }
+
 
     public static EnterOrderRq createNewOrderRq(long requestId, String securityIsin, long orderId, LocalDateTime entryTime, Side side, int quantity, int price, long brokerId, long shareholderId, int peakSize) {
         return new EnterOrderRq(OrderEntryType.NEW_ORDER, requestId, securityIsin, orderId, entryTime, side, quantity, price, brokerId, shareholderId, peakSize);
